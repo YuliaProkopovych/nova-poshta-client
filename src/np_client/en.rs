5db6cd806728a8067127ad36bb02bb07f306787c
YuliaProkopovych <yuliaprokopovych@gmail.com>
2023-10-05 16:09:15 +0300

    Fix EN number conversion

--- src/np_client/en.rs
+++ src/np_client/en.rs
@@ -31,12 +31,15 @@ impl TryFrom<&str> for ENumber {
     type Error = String;
 
     fn try_from(value: &str) -> Result<Self, Self::Error> {
+        if value.len() != 14 {
+            return Err("Invalid value".to_owned());
+        }
         let email_regex = Regex::new(r"[0-9+]").unwrap();
 
-        if email_regex.is_match(value) {
+        if email_regex.is_match(&value) {
             Ok(Self(value.to_owned()))
         } else {
-            Err("Cannot convert from string".to_owned())
+            Err("Invalid value".to_owned())
         }
     }
 }
